467c6405ed158549413fb874191192d4b10eec88
DANIEL HOYOS CORREA <dantethemaster034@gmail.com>
2023-02-04 00:08:00 -0500

    Dash finished

--- GameJam/Assets/Scripts/Movement/PlayerController.cs
+++ GameJam/Assets/Scripts/Movement/PlayerController.cs
@@ -1,9 +1,10 @@
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private float speed = 8f, jumpForce = 16f, jumpForcePressed = 0.5f;
+    [SerializeField] private float speed = 8f, jumpForce = 16f, jumpForcePressed = 0.5f, dashForce = 24f, dashCd = 1f;
     [SerializeField] private SpriteRenderer spRenderer;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groudLayer;
@@ -11,14 +12,23 @@ public class PlayerController : MonoBehaviour
     //UTILIDAD
     private Vector2 axis = Vector2.zero;
     private byte zero = 0;
-    private float groundCollRad = 0.2f;
-    
+    private float groundCollRad = 0.2f, dashTime = 0.2f, originalGravity = 1f;
+    private bool isDashing = false, canDash = true;
+
+    private void Awake()
+    {
+        originalGravity = rb.gravityScale;
+    }
+
     // Update is called once per frame
     void Update()
     {
         InputDetection();
-        Move();
-        Jump();
+        if (!isDashing)
+        {
+            Move();
+            Jump();
+        }
     }
 
     #region Methods
@@ -27,6 +37,8 @@ public class PlayerController : MonoBehaviour
     {
         //Modifica la velocidad del cuerpo rigido del jugador para moverlo respectivamente
         rb.velocity = new Vector2(axis.x * speed, rb.velocity.y);
+        
+        Dash();
     }
 
     void Jump()
@@ -63,11 +75,35 @@ public class PlayerController : MonoBehaviour
             spRenderer.flipX = false;
         }
     }
+    
+    void Dash()
+    {
+        if (Input.GetButtonDown("Dash") && axis.x != zero && canDash)
+        {
+            isDashing = true;
+            canDash = false;
+            rb.gravityScale = 0;
+            rb.velocity = new Vector2(dashForce * axis.x, 0);
+            Invoke("DashReset", dashTime);
+            Invoke("DashCd", dashCd);
+        }
+    }
 
     bool IsGrounded()
     {
         //Revisa la permanencia del jugador 
         return Physics2D.OverlapCircle(groundCheck.position, groundCollRad, groudLayer);
+    }
+
+    void DashReset()
+    {
+        isDashing = false;
+        rb.gravityScale = originalGravity;
+    }
+
+    void DashCd()
+    {
+        canDash = true;
     }
 
     #endregion
